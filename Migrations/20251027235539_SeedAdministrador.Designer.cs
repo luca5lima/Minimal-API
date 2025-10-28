5e9d733765412df4118cfcc3eddbb17cbfdcf332
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2025-10-27 21:01:02 -0300

    Criando Seed para cadastrar administrador padão

--- Migrations/20251027235539_SeedAdministrador.Designer.cs
+++ Migrations/20251027235539_SeedAdministrador.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using minimalApi.Infraestrutura.Db;
 
@@ -10,9 +11,11 @@ using minimalApi.Infraestrutura.Db;
 namespace Minimal_API.Migrations
 {
     [DbContext(typeof(DbContexto))]
-    partial class DbContextoModelSnapshot : ModelSnapshot
+    [Migration("20251027235539_SeedAdministrador")]
+    partial class SeedAdministrador
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- obj/Debug/net9.0/Minimal-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Minimal-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minimal-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda6d4a1101557c20f3896223f1781cb9c299351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba099b026d818eca9958208f5fee178f112585c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minimal-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minimal-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
