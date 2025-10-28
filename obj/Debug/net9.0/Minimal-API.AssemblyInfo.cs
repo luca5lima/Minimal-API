49ee527d030c6b4c99541f2d0def1153aff142fc
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2025-10-28 13:38:58 -0300

    POST para criar veiculo

--- obj/Debug/net9.0/Minimal-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Minimal-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minimal-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d01870c2bc0aff947eac47ff5f1eae478b932bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28b5a551740a35deb06e75532afa40f27515f9fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minimal-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minimal-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
