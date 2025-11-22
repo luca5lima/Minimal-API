ffe171f82915f9dd8f16258a543440a7bd2f8dc0
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2025-11-22 17:44:34 -0300

    Criando tests de Request das Rotas de Administradores

---
+++ Test/Mocks/AdministradorServicoMock.cs
@@ -0,0 +1,45 @@
+using minimalApi.Dominio.Entidades;
+using minimalApi.Dominio.Interfaces;
+using minimalApi.DTOs;
+
+namespace Test.Mocks;
+
+public class AdministradorServicoMock : IAdministradorServico
+{
+    private static List<Administrador> administradores = new List<Administrador>(){
+        new Administrador{
+            Id = 1,
+            Email = "adm@teste.com",
+            Senha = "123456",
+            Perfil = "Adm"
+        },
+        new Administrador{
+            Id = 2,
+            Email = "editor@teste.com",
+            Senha = "123456",
+            Perfil = "Editor"
+        }
+    };
+    
+    public Administrador? BuscaPorId(int id)
+    {
+        return administradores.FirstOrDefault(a => a.Id == id);
+    }
+
+    public Administrador Incluir(Administrador administrador)
+    {
+        administrador.Id = administradores.Count() + 1;
+        administradores.Add(administrador);
+        return administrador;
+    }
+
+    public Administrador? Login(LoginDTO loginDTO)
+    {
+        return administradores.Find(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha);
+    }
+
+    public List<Administrador> Todos(int? pagina)
+    {
+        return administradores;
+    }
+}
